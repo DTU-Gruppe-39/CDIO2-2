d8d4a449bcda7332cfcca7fac311d1c6db642f17
Snowspect <s155384@student.dtu.dk>
2018-03-08 20:47:15 +0100

    DTO get and sets should work now

--- src/DTO.java
+++ src/DTO.java
@@ -3,8 +3,8 @@ public class DTO {
 	
 	String operatorId;
 	String batchId;
-	String taraWeight, netWeight, bruttoWeight; 
-	//double netWeight, bruttoWeight, taraWeight;	
+	//String taraWeight, netWeight, bruttoWeight; 
+	double netWeight, bruttoWeight, taraWeight;	
 	
 	
 
@@ -29,78 +29,78 @@ public class DTO {
 
 
 
-
+	
 	public void setBatchId(String batchId) {
 		this.batchId = batchId;
 	}
 
 
-
+	/*
 	public String getTaraWeight()
 	{
 		return taraWeight;
 	}
+	*/
 	
-	/*
 	public double getTaraWeight() {
 		return taraWeight;
 	}
-	 */
+	
 
 
-
+	/*
 	public void setTaraWeight(String taraWeight)
 	{
 		this.taraWeight = taraWeight;
 	}
-	/*
+	*/
 	public void setTaraWeight(double taravWeight) {
 		this.taraWeight = taravWeight;
 	}
-	 */
+	 
 
-
+	/*
 	public String getNetWeight()
 	{
 		return netWeight;
 	}
-	/*
+	*/
 	public double getNetWeight() {
 		return netWeight;
 	}
-	*/
+	
 
-
+	/*
 	public void setNetWeight(String netWeight)
 	{
 		this.netWeight = netWeight;
 	}
-	/*
+	*/
 	public void setNetWeight(double netWeight) {
 		this.netWeight = netWeight;
 	}
-	*/
-
+	
+	/*
 	public String getBruttoWeight()
 	{
 		return bruttoWeight;
 	}
-	/*
+	*/
 	public double getBruttoWeight() {
 		return bruttoWeight;
 	}
-	*/
+	
 
-
+	/*
 	public void setBruttoWeight(String bruttoWeight)
 	{
 		this.bruttoWeight = bruttoWeight;
 	}
-	/*
+	*/
 	public void setBruttoWeight(double bruttoWeight) {
 		this.bruttoWeight = bruttoWeight;
 	}
-	*/
+	
 
 	public String toString(){
 		return "operat�rId: " + operatorId+ " batchId: " + batchId
