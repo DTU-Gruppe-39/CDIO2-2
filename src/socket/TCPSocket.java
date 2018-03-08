92811c243b67c7a8a9700a766044b429045348ce
rallerG <raller799@live.dk>
2018-03-08 13:04:08 +0100

    socket example

--- src/socket/TCPSocket.java
+++ src/socket/TCPSocket.java
@@ -1,5 +1,20 @@
 package socket;
+import java.io.*;
+import java.net.*;
 
 public class TCPSocket {
-
+	
+	public void clientTCPSocket() throws Exception {
+		String sentence;
+		String modifiedSentence;
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		Socket clientSocket = new Socket("localhost", 6789);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		sentence = inFromUser.readLine();
+		outToServer.writeBytes(sentence + '\n');
+		modifiedSentence = inFromServer.readLine();
+		System.out.println("FROM SERVER: " + modifiedSentence);
+		clientSocket.close();
+	}
 }
