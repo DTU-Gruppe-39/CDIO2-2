3998136a5458debe69eef9d9a451f425b399f0ca
Brian Pedersen <32457177+Lhabibi@users.noreply.github.com>
2018-03-08 14:05:11 +0100

    dto - uden tostring

--- src/DTO.java
+++ src/DTO.java
@@ -1,4 +1,42 @@
 
 public class DTO {
+	
+	String operatoerId;
+	int batchId;
+	double taravaegt, nettovaegt, bruttovaegt;	
+	
+	public String getOperatoerId() {
+		return operatoerId;
+	}
+	public void setOperatoerId(String operatoerId) {
+		this.operatoerId = operatoerId;
+	}
+	public int getBatchId() {
+		return batchId;
+	}
+	public void setBatchId(int batchId) {
+		this.batchId = batchId;
+	}
+	public double getTaravaegt() {
+		return taravaegt;
+	}
+	public void setTaravaegt(double taravaegt) {
+		this.taravaegt = taravaegt;
+	}
+	public double getNettovaegt() {
+		return nettovaegt;
+	}
+	public void setNettovaegt(double nettovaegt) {
+		this.nettovaegt = nettovaegt;
+	}
+	public double getBruttovaegt() {
+		return bruttovaegt;
+	}
+	public void setBruttovaegt(double bruttovaegt) {
+		this.bruttovaegt = bruttovaegt;
+	}
 
+	
+	
+	
 }
