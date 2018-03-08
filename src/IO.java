1c5e3d2888acae71286dd789112fa42c3e8c0739
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2018-03-08 14:51:59 +0100

    Modified connection class

--- src/IO.java
+++ src/IO.java
@@ -1,9 +1,20 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import socket.Connection;
 import socket.TCPSocket;
 
 public class IO {
 
-	public IO(TCPSocket tcp, DTO dto) {
+	public IO(TCPSocket tcp, DTO dto) throws UnknownHostException, IOException {
 		// TODO Auto-generated constructor stub
+		Connection conn = new Connection("127.0.0.1",8000);
+		Socket clientSocket = conn.SocketConn();
+		DataOutputStream sendToServer = conn.getWriter();
+		BufferedReader getFromServer = conn.getReader();
 	}
 
 }
--- src/socket/Connection.java
+++ src/socket/Connection.java
@@ -4,9 +4,11 @@ import java.net.*;
 
 public class Connection 
 {
-	int port = 0;
-	String LocalHost = "";
-	Socket clientSocket = null;
+	private BufferedReader br;
+	private DataOutputStream dos;
+	private int port = 0;
+	private String LocalHost = "";
+	private Socket clientSocket = null;
 
 	/**
 	 * Doesn't create connection but fills out values for when you want to make
@@ -22,7 +24,17 @@ public class Connection
 	 */
 	public Socket SocketConn () throws UnknownHostException, IOException
 	{
-		clientSocket = new Socket(LocalHost, port);
+		this.clientSocket = new Socket(this.LocalHost, this.port);
 		return clientSocket;
+	}
+	
+	public BufferedReader getReader() throws IOException {
+		br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		return br;
+	}
+	
+	public DataOutputStream getWriter() throws IOException{
+		dos = new DataOutputStream(clientSocket.getOutputStream());
+		return dos;
 	}
 }
